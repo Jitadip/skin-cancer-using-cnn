212321caa3c3bdda79e631e292d21f500ca33d1d
Jitadip <52451726+Jitadip@users.noreply.github.com>
2025-06-15 20:50:03 +0530

    app.py

--- app (1).py
+++ app (1).py
@@ -4,7 +4,6 @@ import pandas as pd
 from PIL import Image
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.image import img_to_array
-
 from keras.layers import Softmax
 
 model = load_model('model.keras')
@@ -17,18 +16,15 @@ if uploaded is not None:
     image = Image.open(uploaded)
     st.image(image, caption="Input Cell Image", width=200)
 
-
     def preprocess_image(uploaded_image):
         resized_image = uploaded_image.resize((256, 256))
         image_array = img_to_array(resized_image)
         image_array /= 255.
         return image_array
 
-
     def prediction(image_array):
         pred = model.predict(np.expand_dims(image_array, axis=0))
         return pred
-
 
     inp = preprocess_image(image)
     ans = prediction(inp)
